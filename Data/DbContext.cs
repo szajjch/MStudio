c7e487f68e9cddfe4ac558b31d00e0a568f5d5cd
Daniel Kasprzak <gamecast217@gmail.com>
2024-02-25 14:52:41 +0100

    EF Data
    dbContext

---
+++ Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using barber_website.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace barber_website.Data
+{
+    public class ReservationDbContext : DbContext
+    {
+        public ReservationDbContext(DbContextOptions<ReservationDbContext> options) : base(options) { }
+        
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<OpeningHours> OpeningHours { get; set; }
+
+
+        // Dodać confirmation code
+    }
+}
