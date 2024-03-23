789f0e7a161c12765bb5205025ea33863e572960
Daniel Kasprzak <gamecast217@gmail.com>
2024-03-23 19:21:52 +0100

    Booking service init
    - Bool task to check available slots
    - Task to book specified slot - init

---
+++ Services/Book.cs
@@ -0,0 +1,35 @@
+﻿using barber_website.Data;
+using barber_website.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace barber_website.Services
+{
+	public class Book
+	{
+		private readonly ReservationDbContext _reservationDbContext;
+
+		public Book(ReservationDbContext reservationDbContext)
+		{
+			_reservationDbContext = reservationDbContext;
+		}
+
+		public async Task<bool> IsSlotAvailable(DateTime resDateTime, int duration)
+		{
+			var existingReservation = await _reservationDbContext.Reservations
+				.Where(r => r.ReservationDateTime <= resDateTime && r.ReservationDateTime.AddMinutes(r.Duration) >= resDateTime)
+				.FirstOrDefaultAsync();
+
+			return existingReservation == null;
+		}
+
+		public async Task BookHour(Reservation res)
+		{
+
+
+
+
+			_reservationDbContext.Add(res);
+			await _reservationDbContext.SaveChangesAsync();
+		}
+	}
+}
