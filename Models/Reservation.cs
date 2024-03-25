90805f00b70a379b569b4d650ccc5ca08f8d8db5
Daniel Kasprzak <gamecast217@gmail.com>
2024-03-25 16:47:14 +0100

    Model changes
    - Removed age
    - Added phone
    - Changed ReservationType into Services

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -1,12 +1,15 @@
 ﻿namespace barber_website.Models
 {
+	// Zostawic firstname
+	// Dodac json dla wszystkich klientow
+
     public class Reservation
     {
 		public string ReservationId { get; set; }
 		public string FirstName { get; set; }
-		public int Age { get; set; }
 		public string Email { get; set; }
-		public string ReservationType { get; set; }
+		public string Phone { get; set; }
+		public string Services { get; set; }
 		public int Duration { get; set; }
 		public DateTime ReservationDateTime { get; set; }
 	}
