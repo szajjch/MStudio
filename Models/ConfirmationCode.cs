1fecc2e04eceb9fc4b7178aff717950ef78b1410
Daniel Kasprzak <gamecast217@gmail.com>
2024-03-27 19:21:54 +0100

    Proper model

--- Models/ConfirmationCode.cs
+++ Models/ConfirmationCode.cs
@@ -2,13 +2,8 @@
 {
     public class ConfirmationCode
     {
-		public int ConfirmationCodeId { get; set; }
 		public string Email { get; set; }
 		public string Code { get; set; }
 		public DateTime CreatedAt { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string ReservationType { get; set; }
-		public DateTime ReservationDateTime { get; set; }
 	}
 }
