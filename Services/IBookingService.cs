affc5519522ea95a0b00286dd1f8d734f2e92161
Daniel Kasprzak <gamecast217@gmail.com>
2024-03-28 15:56:43 +0100

    Interface

---
+++ Services/IBookingService.cs
@@ -0,0 +1,12 @@
+﻿using barber_website.Models;
+
+namespace barber_website.Services
+{
+	public interface IBookingService
+	{
+		Task<bool> IsReservationAvailable(DateTime resDateTime, int duration, string email);
+		Task BookHour(Reservation res);
+		Task StartVerification(string email);
+		string GenerateCode(int length);
+	}
+}
