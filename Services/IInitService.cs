10d977014fc2a1a24ac5e433f2f62271a7cef426
Daniel Kasprzak <gamecast217@gmail.com>
2024-04-04 22:21:00 +0200

    Initialization service + inferface

---
+++ Services/IInitService.cs
@@ -0,0 +1,9 @@
+﻿using barber_website.Models;
+
+namespace barber_website.Services
+{
+	public interface IInitService
+	{
+		Task<List<Offer>> GetOffers();
+	}
+}
---
+++ Services/InitService.cs
@@ -0,0 +1,21 @@
+﻿using barber_website.Data;
+using barber_website.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace barber_website.Services
+{
+	public class InitService : IInitService
+	{
+		private readonly ReservationDbContext _reservationDbContext;
+
+		public InitService(ReservationDbContext reservationDbContext)
+		{
+			_reservationDbContext = reservationDbContext;
+		}
+
+		public async Task<List<Offer>> GetOffers()
+		{
+			return await _reservationDbContext.Offers.ToListAsync();
+		}
+	}
+}
