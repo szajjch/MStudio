88c62bb7f31322b56eaa4fb2e302b86135c9d7c2
Daniel Kasprzak <gamecast217@gmail.com>
2024-04-04 22:20:32 +0200

    Dynamic offers model

---
+++ Models/Offer.cs
@@ -0,0 +1,11 @@
+﻿namespace barber_website.Models
+{
+	public class Offer
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int Duration { get; set; }
+		public decimal Price { get; set; }
+	}
+}
