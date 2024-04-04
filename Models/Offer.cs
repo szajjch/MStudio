121eb4db575a28bde94742d9023ff202948fb891
Daniel Kasprzak <gamecast217@gmail.com>
2024-04-04 22:32:37 +0200

    Model change

--- Models/Offer.cs
+++ Models/Offer.cs
@@ -3,7 +3,7 @@
 	public class Offer
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
+		public string Label { get; set; }
 		public string Description { get; set; }
 		public int Duration { get; set; }
 		public decimal Price { get; set; }
