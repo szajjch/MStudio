6022d5dccd08edb948ff59d944e3f126c9102a17
Daniel Kasprzak <gamecast217@gmail.com>
2024-04-19 21:16:36 +0200

    OtherDays db model

---
+++ Models/OtherDays.cs
@@ -0,0 +1,10 @@
+﻿namespace barber_website.Models
+{
+	public class OtherDays
+	{
+		public DateOnly date {  get; set; }
+		public bool isOpen { get; set; }
+		public TimeSpan openHour { get; set; }
+		public TimeSpan closeHour { get; set; }
+	}
+}
